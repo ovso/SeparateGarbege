0d0280c6cc1436ca7b99f7aca3cec647bae1bc8c
ohjaeho <ovsoce@gmail.com>
2017-06-27 09:06:03 +0900

    Remove EasyPreferences

--- app/src/main/java/io/github/ovso/separategarbege/PhotoViewActivity.java
+++ app/src/main/java/io/github/ovso/separategarbege/PhotoViewActivity.java
@@ -1,17 +1,13 @@
 package io.github.ovso.separategarbege;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.ImageView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 import com.bumptech.glide.Glide;
 import com.github.chrisbanes.photoview.PhotoView;
-import com.pixplicity.easyprefs.library.Prefs;
 
 /**
  * Created by jaeho on 2017. 6. 26
@@ -29,12 +25,5 @@ public class PhotoViewActivity extends AppCompatActivity {
       String url = getIntent().getStringExtra("url");
       Glide.with(this).load(url).into(mPhotoView);
     }
-    boolean isVisible = Prefs.getBoolean("GuideVisible", true);
-    mPinchZoomImageview.setVisibility(isVisible?View.VISIBLE:View.GONE);
-  }
-
-  @OnClick(R.id.pinch_zoom_imageview) void onPinchZoomClick() {
-    Prefs.putBoolean("GuideVisible", false);
-    mPinchZoomImageview.setVisibility(View.GONE);
   }
 }
